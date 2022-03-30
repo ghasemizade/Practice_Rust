6fbb48233c2b6303cf057a3a17d02564c711553b
Hosein <mrqasemizade@gmail.com>
2022-03-30 14:47:33 +0430

    draw circle 2022

---
+++ draw-circle 2022/draw-circle.rs
@@ -0,0 +1,12 @@
+use turtle::Turtle;
+//draw circle with turtle
+fn main() {
+    let mut turtle = Turtle::new();
+
+    for _ in 0..360 {
+
+        turtle.forward(3.0);
+        
+        turtle.right(1.0);
+    }
+}
